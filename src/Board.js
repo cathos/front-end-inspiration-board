8de656422deb163621a99befd7ee5aa5a0cc3d33
Hillary Smith <hillaryhsmith@gmail.com>
2022-07-12 11:14:48 -0700

    attempt to start connecting backend and frontend

--- src/Board.js
+++ src/Board.js
@@ -1,19 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+// import axios from "axios";
+
+
 
 const Board = ({ boards, handleChange }) => {
+  const [boardList, setBoardList] = useState([])
+  const axios = require("axios");
+  axios.get("https://orange-purple-inspo-board.herokuapp.com/boards")
+  .then(function(response) {
+    response = response.data
+    setBoardList(response)
+  // useEffect = (() => {
+
+  //   }, [])
   return (
     <section className="displayBoards">
       <label>☁️Boards☁️</label>
       <select className="dropdown" onClick={handleChange}>
-        {boards.map((board) => {
+        {boardList.map((board) => {
           //got an array wanted an object error...
           return <option>{`Title: ${board.title} By: ${board.owner}`}</option>;
         })}
       </select>
     </section>
   );
-};
+})};
+
 
 Board.propTypes = {
   boards: PropTypes.arrayOf(
