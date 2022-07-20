cc6ab55adc2701a5e1101266414be299b49c8627
Hillary Smith <hillaryhsmith@gmail.com>
2022-07-20 09:30:51 -0700

    Added content to Home and About pages

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -3,7 +3,15 @@ import React from "react";
 const Home = () => {
   return (
     <div className="App">
-      <h1>InspoBoard Home Page</h1>
+      <div id="Home"><h1>InspoBoard Home Page</h1>
+      <p>
+        Welcome to inspiration board!
+        This is a digital board where you can post kind words, encouraging messages, inspirational quotes, jokes... anything you want that might make your day better. 
+        You can create different boards, each with their own corresponding cards. 
+        If someone else likes what the cards say they can give it some love, with our likes counter. 
+        Please use and enjoy for all your inspiration needs!
+      </p>
+      </div>
     </div>
   );
 };
