62be6226e13bb7041c267adfecc0f85e96a14d32
Hillary Smith <hillaryhsmith@gmail.com>
2022-07-22 08:26:17 -0700

    fixed mixed content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useReducer, useEffect } from "react";
 import "./App.css";
 import "./dropdown.css";
 import axios from "axios";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
 import About from "./Pages/about";
 import BoardsPage from "./Pages/boards";
 import Home from "./Pages/home";
@@ -13,37 +13,7 @@ import "./Styles/header.css";
 import NotFound from "./Pages/NotFound";
 import Selected from "./Pages/Selected";
 import Footer from "./Components/ui/Footer";
-
 function App() {
-  //BOARDS
-  const boardApiToJson = (board) => {
-    const { id, title, owner } = board;
-    return { id, title, owner };
-  };
-
-  //Delete Board Route
-  const deleteABoard = (id) => {
-    return axios
-      .delete(`https://orange-purple-inspo-board.herokuapp.com/boards/${id}`)
-      .then((response) => {
-        return boardApiToJson(response.data);
-      });
-  };
-
-  const [prompt, setPrompt] = useState("Select Board...");
-
-  //delete board
-  const removeBoard = (id) => {
-    deleteABoard(id).then((updatedBoard) => {
-      const newBoardData = boards.filter((board) => {
-        return board.id !== id;
-      });
-      console.log("i'm inside remove board");
-      console.log(`value: ${value}`);
-      setBoards(newBoardData);
-      setPrompt("Select Board...");
-    });
-  };
   //GET cards by board Id
   const displayCards = async (id) => {
     const response = await axios.get(
@@ -106,13 +76,17 @@ function App() {
     const response = await axios.get(
       "https://orange-purple-inspo-board.herokuapp.com/boards"
     );
-    console.log("boards and such");
     setBoards(response.data);
   };
 
   useEffect(() => {
     getBoards();
   }, []);
+
+  const BOARD_ACTIONS = {
+    DELETE_BOARD: "deleteBoard",
+    ADD_CARD: "addCard",
+  };
 
   //CARDS
   const cardApiToJson = (card) => {
@@ -121,23 +95,25 @@ function App() {
   };
 
   //PATCH route to increase likes -updateAPI
-  const incLikes = (id) => {
-    return axios
-      .patch(
-        `https://orange-purple-inspo-board.herokuapp.com/cards/${id}/increase_likes_count`
-      )
+  const incLikes = async (id) => {
+    const response = await axios
+      .patch(`cards/${id}/increase_likes_count`)
       .then((response) => {
         return cardApiToJson(response.data);
       });
+    console.log(response.data);
+    // setCards(response.data)
   };
 
-  //DELETE route to delete Card -update api
-  const deleteCard = (id) => {
-    return axios
-      .delete(`https://orange-purple-inspo-board.herokuapp.com/cards/${id}`)
+  //PATCH route to decrease likes -update API
+  const decLikes = async (id) => {
+    const response = await axios
+      .patch(`cards/${id}/decrease_likes_count`)
       .then((response) => {
         return cardApiToJson(response.data);
       });
+    console.log(response.data);
+    // setCards(response.data)
   };
 
   //Add likes -change state on webpage
@@ -157,15 +133,48 @@ function App() {
     });
   };
 
-  //delete card
-  const removeCard = (id) => {
-    deleteCard(id).then((updatedCard) => {
-      const newCardData = cards.filter((card) => {
-        return card.id !== id;
+  //Minus likes -change state on webpage
+  const minusLikes = (id) => {
+    decLikes(id).then((updatedLikesCount) => {
+      const newLikedData = cards.map((card) => {
+        if (card.id === id) {
+          return {
+            ...card,
+            likes_count: (updatedLikesCount.likes_count = card.likes_count - 1),
+          };
+        } else {
+          return card;
+        }
       });
-      setCards(newCardData);
+      setCards(newLikedData);
     });
   };
+
+  //toggle card likes
+  // const cardLikes = (id) => {
+  //   const newLikedData = cards.map((card) => {
+  //     if (card.id === id) {
+  //       if (card.likes_count === 0) {
+  //         return {
+  //           ...card,
+  //           likes_count: (card.likes_count = card.likes_count + 1),
+  //         };
+  //       } else if (card.likes_count === 1) {
+  //         return {
+  //           ...card,
+  //           likes_count: (card.likes_count = card.likes_count - 1),
+  //         };
+  //       } else {
+  //         return card;
+  //       }
+  //     } else {
+  //       return card;
+  //     }
+  //   });
+  //   //patch route here
+  //   setCards(newLikedData);
+  // };
+
   //adds form elements to dropdown in board component
   //send this state to SelectedBoard
   const [selectedBoard, setSelectedBoard] = useState({
@@ -181,6 +190,32 @@ function App() {
     }
   };
 
+  //Board actions with useReducer
+  //selectedBooards actions
+  //delete board
+  //add card
+  const boardReducer = (selectedBoardState, action) => {
+    switch (action.type) {
+      // case BOARD_ACTIONS.ADD_CARD:
+      // return setCardFormHidden(false);
+      // case BOARD_ACTIONS.DELETE_BOARD:
+      //   return boards.filter((form) => form.id !== action.payload.id);
+      default:
+        return selectedBoardState;
+    }
+  };
+  const [selectedBoardState, selectedBoardDispatch] = useReducer(
+    boardReducer,
+    []
+  );
+  const deleteBoard = () => {
+    selectedBoardDispatch({ type: BOARD_ACTIONS.DELETE_BOARD });
+  };
+
+  const addCard = () => {
+    selectedBoardDispatch({ type: BOARD_ACTIONS.ADD_CARD });
+  };
+
   return (
     <BrowserRouter>
       <Header />
@@ -193,13 +228,12 @@ function App() {
             <Selected
               setSelectedBoard={setSelectedBoard}
               boards={selectedBoard}
+              selectedBoardState={selectedBoardState}
+              deleteBoard={deleteBoard}
+              addCard={addCard}
               cards={cards}
+              minusLikes={minusLikes}
               addLikes={addLikes}
-              incLikes={incLikes}
-              removeCard={removeCard}
-              removeBoard={removeBoard}
-              setPrompt={setPrompt}
-              value={value}
             />
           }
         />
@@ -210,13 +244,11 @@ function App() {
               getSelectedBoard={getSelectedBoard}
               handleChange={boardChange}
               options={boards}
-              // prompt="Select Board"
-              prompt={prompt}
+              prompt="Select Board"
               value={value}
               onChange={(value) => setValue(value)}
               addBoardData={addBoardData}
               displayCards={displayCards}
-              removeBoard={removeBoard}
             />
           }
         />
@@ -229,6 +261,7 @@ function App() {
               boards={boards}
               setBoards={setBoards}
               cards={cards}
+              // postCardToBoard={postCardToBoard}
               selectedBoard={selectedBoard}
               clickedBoard={(value) => setValue(value)}
               addCardToBoard={addCardToBoard}
@@ -241,5 +274,6 @@ function App() {
     </BrowserRouter>
   );
 }
+
 
 export default App;
